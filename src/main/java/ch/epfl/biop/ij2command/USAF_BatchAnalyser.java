0a2b552c88a1f7309f5a622c72a6bbd585347f3f
Seitz Arne <arne.seitz@epfl.ch>
2024-05-13 11:12:46 +0200

    BatchAnalysis added

--- src/main/java/ch/epfl/biop/ij2command/USAF_BatchAnalyser.java
+++ src/main/java/ch/epfl/biop/ij2command/USAF_BatchAnalyser.java
@@ -11,13 +11,11 @@ import ij.IJ;
 import ij.ImagePlus;
 import ij.ImageStack;
 import ij.WindowManager;
-import ij.gui.Line;
 import ij.gui.Roi;
 import ij.measure.CurveFitter;
 import ij.measure.ResultsTable;
 import ij.plugin.filter.MaximumFinder;
 import ij.process.ImageProcessor;
-import ij.process.ImageStatistics;
 import ij.util.ArrayUtil;
 import net.imagej.ImageJ;
 
@@ -29,8 +27,8 @@ import net.imagej.ImageJ;
  * </p>
  */
 
-@Plugin(type = Command.class, menuPath = "Plugins>BIOP>USAF Traget Batch Analyser")
-public class USAF_TragetAnalyser implements Command {
+@Plugin(type = Command.class, menuPath = "Plugins>BIOP>USAF Traget Analyser")
+public class USAF_BatchAnalyser implements Command {
 
 	private int width;
 	private int height;
@@ -75,26 +73,17 @@ public class USAF_TragetAnalyser implements Command {
     public void run() {
     	
     	
-		IJ.run("Bio-Formats Importer", "open=N:/temp-Arne/StageTest/USFA_Slide_Exp13.lsm color_mode=Default rois_import=[ROI manager] view=Hyperstack stack_order=XYCZT use_virtual_stack");
-		
+		Roi rectangle=new Roi(x, y, widthAnalysis, heightAnalysis);
 		ImagePlus imp=WindowManager.getCurrentImage();
-		ImageProcessor ip=imp.getProcessor();
-		ImageStatistics stats=new ImageStatistics();
+		imp.setRoi(rectangle);
 		
-		for (int pos=0;pos<30;pos++) {
-			Roi line=new Line(94+pos*40, 500, 94+pos*40, 540);
-			imp.setRoi(line);
-			stats=imp.getStatistics();
-			IJ.log("Mean: "+stats.mean);
-	        IJ.log("Max: "+stats.max);
-
+		ImageProcessor ip=imp.getProcessor().crop();
+		ip.rotate(angle);
+		//ip.findEdges();
 			
-			
-		}
-		
-		
-		
-		
+		double[] line=meanLine(ip);
+		ContrastAnalyser ca=new ContrastAnalyser(line);
+		ca.showPlot(line);
 		
 		
 		
@@ -194,7 +183,7 @@ public class USAF_TragetAnalyser implements Command {
         // create the ImageJ application context with all available services
         final ImageJ ij = new ImageJ();
         ij.ui().showUI();
-        
-        ij.command().run(USAF_TragetAnalyser.class, true);
+
+        ij.command().run(USAF_BatchAnalyser.class, true);
     }
 }
