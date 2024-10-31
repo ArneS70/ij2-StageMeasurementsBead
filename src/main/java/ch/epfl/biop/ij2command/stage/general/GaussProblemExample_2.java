a32d96c0eab0053818361cd20dcf06f8f615bd08
Seitz Arne <arne.seitz@epfl.ch>
2024-10-31 18:12:23 +0100

    Marquardt AsymGauss

--- src/main/java/ch/epfl/biop/ij2command/stage/general/GaussProblemExample_2.java
+++ src/main/java/ch/epfl/biop/ij2command/stage/general/GaussProblemExample_2.java
@@ -25,19 +25,31 @@ public class GaussProblemExample_2 {
 		List<Double> y = new ArrayList<Double>();
         
         // construct QuadraticFunction
-        GaussFunction qf = new GaussFunction(x,y);
+        AsymGaussFunction qf = new AsymGaussFunction(x,y);
         
         //entry the data
-		qf.addPoint(-10, 1.01);
-		qf.addPoint(-8, 1.07);
-		qf.addPoint(-6, 1.23);
-		qf.addPoint(-4, 1.52);
-		qf.addPoint(-2, 1.85);
-		qf.addPoint(0, 2);
-		qf.addPoint(2, 1.85);
-		qf.addPoint(4, 1.52);
-		qf.addPoint(6, 1.23);
-		qf.addPoint(8, 1.07);
+		qf.addPoint(-10, 1.09);
+		qf.addPoint(-9, 1.12);
+		qf.addPoint(-8, 1.17);
+		qf.addPoint(-7, 1.23);
+		qf.addPoint(-6, 1.31);
+		qf.addPoint(-5, 1.41);
+		qf.addPoint(-4, 1.53);
+		qf.addPoint(-3, 1.67);
+		qf.addPoint(-2, 1.82);
+		qf.addPoint(-1, 1.98);
+		qf.addPoint(0, 2.13);
+		qf.addPoint(1, 2.25);
+		qf.addPoint(2, 2.35);
+		qf.addPoint(3, 2.39);
+		qf.addPoint(4, 2.37);
+		qf.addPoint(5, 2.28);
+		qf.addPoint(6, 2.13);
+		qf.addPoint(7, 1.93);
+		qf.addPoint(8, 1.71);
+		qf.addPoint(9, 1.51);
+		qf.addPoint(10, 1.35);
+		
 
 		//prepare construction of LeastSquresProblem by builder
 		LeastSquaresBuilder lsb = new LeastSquaresBuilder();
@@ -48,11 +60,11 @@ public class GaussProblemExample_2 {
 		
 		//set target data
 		lsb.target(newTarget);
-		double[] newStart = {1,1,1};
+		double[] newStart = {1,1,1,1,1,1};
 		//set initial parameters
 		lsb.start(newStart);
 		//set upper limit of evaluation time
-		lsb.maxEvaluations(10000);
+		lsb.maxEvaluations(100000);
 		//set upper limit of iteration time
 		lsb.maxIterations(1000);
 
@@ -68,6 +80,9 @@ public class GaussProblemExample_2 {
 			System.out.println("A: " + optimalValues[0]);
 			System.out.println("B: " + optimalValues[1]);
 			System.out.println("C: " + optimalValues[2]);
+			System.out.println("D: " + optimalValues[3]);
+			System.out.println("E: " + optimalValues[4]);
+			System.out.println("F: " + optimalValues[5]);
 			System.out.println("Iteration number: "+lsoo.getIterations());
 			System.out.println("Evaluation number: "+lsoo.getEvaluations());
 		} catch (Exception e) {
